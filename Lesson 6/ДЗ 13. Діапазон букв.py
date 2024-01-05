eda37f3f1796c1d523c2d0849c7207acd53b62b3
User <gleb1395@gmail.com>
2024-01-05 15:24:03 +0200

    Second version

--- Lesson 6/ДЗ 13. Діапазон букв.py
+++ Lesson 6/ДЗ 13. Діапазон букв.py
@@ -10,3 +10,15 @@ else:
         fd = string.ascii_letters.find(x)
         index_find.append(fd)
 print(string.ascii_letters[min(index_find):max(index_find) + 1])
+
+# import string
+#
+# user_input_liters = input('Введите две буквы через дефис: ')
+# if len(user_input_liters) > 3:
+#     print('Вы ввели больше символов')
+# a = string.ascii_letters.find(user_input_liters[0])
+# b = string.ascii_letters.find(user_input_liters[2])
+# if a < b:
+#     print(string.ascii_letters[a:b + 1])
+# else:
+#     print(string.ascii_letters[b:a + 1])
