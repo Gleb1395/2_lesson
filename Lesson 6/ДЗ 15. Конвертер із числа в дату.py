7d16e15b16cd639e50a72439dca23145a75533db
User <gleb1395@gmail.com>
2024-01-06 13:21:29 +0200

    Second version

--- Lesson 6/ДЗ 15. Конвертер із числа в дату.py
+++ Lesson 6/ДЗ 15. Конвертер із числа в дату.py
@@ -4,12 +4,13 @@ hour = (user_input_time % 86400) // 3600
 minutes = (user_input_time % 86400 % 3600) // 60
 second = user_input_time % 86400 % 3600 % 60
 if day == 0:
-    print(f'{day} днів, {str(hour).zfill(2)}:{str(minutes).zfill(2)}:{str(second).zfill(2)}')
-elif day == 1 or day % 10 == 1:
-    print(f'{day} день, {str(hour).zfill(2)}:{str(minutes).zfill(2)}:{str(second).zfill(2)}')
+    days_str = 'днів'
+elif day == 1 or day % 10 == 1 and day != 11:
+    days_str = 'день'
 elif day > 9 and day < 20:
-    print(f'{day} днів, {str(hour).zfill(2)}:{str(minutes).zfill(2)}:{str(second).zfill(2)}')
+    days_str = 'днів'
 elif day < 5 or day % 10 < 5:
-    print(f'{day} дні, {str(hour).zfill(2)}:{str(minutes).zfill(2)}:{str(second).zfill(2)}')
+    days_str = 'дні'
 else:
-    print(f'{day} днів, {str(hour).zfill(2)}:{str(minutes).zfill(2)}:{str(second).zfill(2)}')
+    days_str = 'днів'
+print(f'{day} {days_str}, {str(hour).zfill(2)}:{str(minutes).zfill(2)}:{str(second).zfill(2)}')
